654346fcaf416ef824ff097f24b16beadca43863
Benjamin Park <parkbenjamin2@gmail.com>
2017-03-30 17:33:33 -0400

    Update Computato.h

--- Computato/Source/Computato.h
+++ Computato/Source/Computato.h
@@ -12,26 +12,38 @@
 class Variable;
 class Method;
 class Class;
+class Template;
 class Parser;
+
+enum VAR_TYPE { NULL_VAR, QUIS, NUMERUS, VERBA, STATUS, GENUS, GAMMA, FUGE, LINKED };
 
 class Variable
 {
 public:
+	union
+	{
+		std::string value_s;
+		int value_f;
+		bool value_b;
+		VAR_TYPE value_t;
+		Class * linked_class;
+		std::vector<Variable *> arr;
+	};
+
 	std::string key;
-	std::string type;
-	std::string value;
-	Class * linked_class;
-	std::vector<Variable *> arr;
+	VAR_TYPE type;
 	std::vector<int> dim;
 
 	Variable();
-	Variable(std::string k, std::string t);
+	Variable(std::string k, VAR_TYPE t);
+	//~Variable();
 };
 
 class Method
 {
 public:
-	std::map<std::string, std::string> arguements;
+	std::vector<VAR_TYPE> arguement_types;
+	std::vector<std::string> arguement_names;
 	std::string body;
 	std::string key;
 	std::string type;
@@ -51,14 +63,28 @@ public:
 	Class(std::string s);
 };
 
+class Template
+{
+public:
+	std::string key;
+	std::vector<std::string> types;
+	std::string body;
+
+	Template(std::string k, std::vector<std::string> t) {
+		key = k; types = t;
+	}
+};
+
 class Parser
 {
 private:
+	void replaceAll(std::string& str, const std::string& from, const std::string& to);
 	std::map<std::string, Class *> source;
+	std::map<std::string, Template *> templates;
 	bool is_number(const std::string& s);
 	std::vector<std::string> split(std::string s, std::string d = " ");
-	Variable parse(std::string s, Class * cl, std::map<std::string, Variable *> &vars);
-	Variable parse(Method *m, Class * cl, std::map<std::string, Variable *> args);
+	Variable * parse(std::string s, Class * cl, std::map<std::string, Variable *> &vars);
+	Variable * parse(Method *m, Class * cl, std::map<std::string, Variable *> args);
 	std::vector<std::string> line_split(std::string s);
 	Class * Delineate(std::string s);
 	Method * ParseMethod(std::string s, Class * slavus);
